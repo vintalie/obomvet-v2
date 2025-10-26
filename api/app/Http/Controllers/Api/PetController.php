16bb745e2fb92652b885545bd4764d3202422a00
vintalie <marioluizsan@gmail.com>
2025-10-26 12:45:38 -0300

    adicionando disparo da notificação no disparo da emergencia quando criada direto para um pet

--- api/app/Http/Controllers/Api/PetController.php
+++ api/app/Http/Controllers/Api/PetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Pet;
+use App\Events\NovaEmergencia;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -53,6 +54,9 @@ class PetController extends Controller
     public function storeEmergencia(Request $request, Pet $pet)
     {
         $emergencia = $pet->emergencias()->create($request->all());
+
+        // Dispara a notificação push via Pusher
+        NovaEmergencia::dispatch($emergencia);
         return response()->json($emergencia, 201);
     }
 
